51791af6262679f7913d52fef9de73a91a383fac
Quách Thành Long <stephensouth1307@gmail.com>
2025-08-22 10:33:00 +0700

    - upload

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -13,6 +13,10 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");// Di chuyển theo trụ x
+        float y = Input.GetAxis("Vertical");// Di chuyển theo trụ y
+
+        Vector3 direction = new Vector3(x, y, 0); // Tính toán hướng di chuyển
+        transform.position = direction * Speed * Time.deltaTime + transform.position; // Cập nhật vị trí của tàu
     }
 }
